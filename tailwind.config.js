1a93e5143d95bcc6cc9cffc6426564f0b100eb33
Ernesto Leal Isla Zamora <ernestolealislazamora@gmail.com>
2024-10-26 23:02:24 +0200

    Added Tailwind and adapted styles.css to output.css

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './challenges/templates/**/*.html',      // Templates in your challenges app
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
